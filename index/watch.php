8893f691783a4d2f42c8004081239eaf161fd77a
xihale <62087089+xihale@users.noreply.github.com>
2020-10-24 22:49:05 +0800

    Update watch.php

--- index/watch.php
+++ index/watch.php
@@ -1,6 +1,28 @@
 <?php
+header("content-type: text/html; charset=utf-8");
 $filename=$_GET['file'];
 $path=$files_in.$_GET['path'];
+if($_GET['del']==$password)
+{
+	if($files_in==(strtr($path,array("/"=>"","\\"=>""))))
+	{
+		echo "<script>alert(\"根目录不许删除！\");</script>";
+	}
+	else if($filename!="")
+	{
+		if(unlink($fs=$path."/".$filename))
+			echo "<script>alert(\"删除成功！\");</script>";
+		else	
+			echo "<script>alert(\"删除失败！".$fs."\");</script>";
+	}
+	else
+	{
+		if(deldir($fs=$path."/"))
+			echo "<meta http-equiv=\"refresh\" content=\"0;url=../\"><script>alert(\"删除成功！\");</script>";
+		else
+			echo "<script>alert(\"删除失败！\");</script>";
+	}
+}
 if($filename!=""&&file_exists($path.$filename)){
 	$fp=fopen($path.$filename,"r");
 	$file=fread($fp,filesize($path.$filename));
@@ -11,5 +33,29 @@ if($filename!=""&&file_exists($path.$filename)){
 }
 else if($filename!=""){
 	echo "<script>alert(\"".$filename."不存在!\")</script>";
+}	
+
+
+//删除指定文件夹以及文件夹下的所有文件
+function deldir($dir) {
+   //先删除目录下的文件：
+   $dh=opendir($dir);
+   while ($file=readdir($dh)) {
+      if($file!="." && $file!="..") {
+         $fullpath=$dir."/".$file;
+         if(!is_dir($fullpath)) {
+            unlink($fullpath);
+         } else {
+            deldir($fullpath);
+         }
+      }
+   }
+   closedir($dh);
+   //删除当前文件夹：
+   if(rmdir($dir)) {
+      return true;
+   } else {
+      return false;
+   }
 }
 ?>
