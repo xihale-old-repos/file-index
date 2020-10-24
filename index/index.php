ee39f87bdc7fa7be4df7b2ecad89248babe707e2
xihale <62087089+xihale@users.noreply.github.com>
2020-10-24 21:56:00 +0800

    Update index.php

--- index/index.php
+++ index/index.php
@@ -3,12 +3,12 @@ $dir=getDir($path);
 echo "项目<br>";
 foreach($dir as $software) //遍历数组并输出
 {
-	echo "<a href=\"/?path=".$_GET['path'].$software."\\\">".$software."</a><br>";
+	echo "<a href=\"/?path=".$_GET['path'].$software."\">".$software."</a><br>";
 }
 $filename=getFile($path);
 echo "文件<br>";
 foreach($filename as $software) //遍历数组并输出
 {
-	echo "<a href=\"/?file=".$_GET['path'].$software."\" target=\"_blank\">".$software."</a><br>";
+	echo "<a href=\"/?file=".$_GET['path']."/".$software."\" target=\"_blank\">".$software."</a><br>";
 }
 ?>
