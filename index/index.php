a03d204d90bee2e0728b3ba4154b94cd3d298044
xihale <62087089+xihale@users.noreply.github.com>
2020-10-25 13:35:58 +0800

    Update index.php

--- index/index.php
+++ index/index.php
@@ -2,17 +2,28 @@
 if(!is_dir($path))
 {
 	 echo "<meta http-equiv=\"refresh\" content=\"0;url=../\">";
+	 exit();
 }
 $dir=getDir($path);
 echo "项目<br>";
 foreach($dir as $software) //遍历数组并输出
 {
-	echo "<a href=\"/?path=".$_GET['path']."/".$software."\">".$software."</a><br>";
+	echo "<a href=\"/?path=".$_GET['path']."/".$software."\">".$software."</a>";
+	if($_COOKIE['write']==$cookie&&$software!="")
+	{
+		echo "&nbsp;&nbsp;<a href=\"/?path=".$_GET['path']."/".$software."&del=".$_COOKIE['write']."\">删除此项目</a>";
+	}
+	echo "<br>";
 }
 $filename=getFile($path);
 echo "文件<br>";
 foreach($filename as $software) //遍历数组并输出
 {
-	echo "<a href=\"/?file=".$_GET['path']."/".$software."\" target=\"_blank\">".$software."</a><br>";
+	echo "<a href=\"/?file=".$_GET['path']."/".$software."\" target=\"_blank\">".$software."</a>";
+		if($_COOKIE['write']==$cookie&&$software!="")
+	{
+		echo "&nbsp;&nbsp;<a href=\"/?file=".$_GET[''].$_GET['file']."/".$software."&del=".$_COOKIE['write']."\">删除此文件</a>";
+	}
+	echo "<br>";
 }
 ?>
