2f180942a9b4dcee57fa882def005a34fab18d60
xihale <62087089+xihale@users.noreply.github.com>
2020-10-30 19:41:20 +0800

    Update write.php

--- write/write.php
+++ write/write.php
@@ -1,64 +1,47 @@
 <?php
-function scrs($str)
+$dir=$_POST['dir'];
+$filename=$_POST['filename'];
+$files=$_POST['files'];
+if($_POST['zhuxiao']!="")
 {
-	return strtr($str,array("/"=>""," "=>"","\\"=>""));
+	setcookie($cookie_name,$cookie,time()-3600);
+	?>
+	<script>alert("注销成功")</script>
+	<meta http-equiv="refresh" content="0;url=../write">
+	<?php
 }
-header("content-type: text/html; charset=utf-8");
-$filename=$_GET['file'];
-$path=$files_in.$_GET['path'];
-if($_GET['del']==$password||$_GET['del']==$cookie)
+if($_COOKIE['write']!=$cookie){
+	exit();
+}?>
+<form action="index.php" method="post">
+	<input type="text" name="dir" placeholder="项目名" value=<?php echo $dir?> />
+	<input type="submit" value="创建" />
+	<input type="submit" value="注销" name="zhuxiao" />
+	<input type="text" name="filename" placeholder="文件名"/><br>
+	<textarea style="height:800px;width:900px" placeholder="文件内容" name="files" /><?php echo $files ?></textarea>
+</form>
+<?php
+if($dir!=''&&$dir!='/')
 {
-	if(scrs($_GET['path'])!=""&&scrs($path)==scrs($files_in))
+	if(!is_dir("../".$files_in.$dir))
 	{
-		echo "<script>alert(\"根目录不许删除！\");</script>";
+		$res=mkdir("../".$files_in."/".$dir,0777,true); 
+		if(!$res)
+			echo "<script>alert(\"项目创建失败\")</script>";
+		else
+			echo "<script>alert(\"目录创建成功\")</script>";
 	}
-	else if($filename!=""&&file_exists($files_in.$filename))
+/*	else
 	{
-		if(!unlink($fs=$path."/".$filename))
-			echo "<script>alert(\"删除失败！\");</script>";
-	}
-	else if(scrs($_GET['path']!=""))
-	{
-		if(!deldir($fs=$path."/"))
-			echo "<script>alert(\"删除失败！\");</script>";
-	}
-}elseif($_GET['del']!=""){
-		echo "<script>alert(\"您不是管理员或登录失效！\")</script>";
+		echo "<script>alert(\"项目已存在\")</script>";
+	}*/
 }
-if($filename!=""&&file_exists($path.$filename)){
-	$fp=fopen($path.$filename,"r");
-	$file=fread($fp,filesize($path.$filename));
-	$file=strtr($file,array("<"=>"&lt;",">"=>"&gt;"));
-	echo "<pre>\n".$file."</pre>";
+if($filename!='')
+{
+	$file="../".$files_in."/".$dir."/".$filename;
+	$fp=fopen($file,"w+");
+	fwrite($fp,$files);
 	fclose($fp);
-	exit();
-}
-else if($filename!=""){
-	echo "<meta http-equiv=\"refresh\" content=\"0;url=../\">";
-	exit();
-}	
-
-
-//删除指定文件夹以及文件夹下的所有文件
-function deldir($dir) {
-   //先删除目录下的文件：
-   $dh=opendir($dir);
-   while ($file=readdir($dh)) {
-      if($file!="." && $file!="..") {
-         $fullpath=$dir."/".$file;
-         if(!is_dir($fullpath)) {
-            unlink($fullpath);
-         } else {
-            deldir($fullpath);
-         }
-      }
-   }
-   closedir($dh);
-   //删除当前文件夹：
-   if(rmdir($dir)) {
-      return true;
-   } else {
-      return false;
-   }
+	echo "<script>alert(\"文件操作成功\")</script>";
 }
 ?>
